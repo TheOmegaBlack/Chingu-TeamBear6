9f392b6ea578428b81add0f56e5594499c0b142d
Sacha <sachamorgese@gmail.com>
2018-03-15 08:07:07 +0900

    fix: fixed eslint (ignoring the front-end build folder) fix: linted the file

--- install-frontend.js
+++ install-frontend.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 const { execSync } = require('child_process')
+const os = require('os')
 
 const frontendGitUrl = 'https://github.com/chingu-voyage4/Bears-Team-6.git'
 const dirName = 'Bears-Team-6'
@@ -9,33 +10,40 @@ const branchName = 'current-sprint'
 const print = (str) => console.log(`${str}\n`)
 const printInfo = (str) => print(`   ... ${str}`)
 const execAndPrint = (command) => print(execSync(command))
+const opSys = os.platform() === 'win32' ? 'windows' : 'mac'
+
+const codeLines = {
+  removeRepo: {
+    mac: 'rm -rf ./Bears-Team-6',
+    windows: 'del "Bears-Team-6"',
+  },
+  removeBuild: {
+    mac: 'rm -rf ./static',
+    windows: 'del "static"',
+  },
+  cloneFresh: `git clone -b ${branchName} ${frontendGitUrl}`,
+  moveRepo: {
+    mac: `mv ./${dirName}/build ./static`,
+    windows: `move "${dirName}/build" "static"`,
+  },
+}
+
+console.log(opSys)
+console.log(codeLines.removeRepo[opSys])
+console.log(codeLines.removeBuild[opSys])
+console.log(codeLines.moveRepo[opSys])
 
 // May not work on Windows OS. should be fixed
 
 const installFrontendScript = () => {
   printInfo('Removing previous frontend repo')
-  execAndPrint('rm -rf ./Bears-Team-6')
-
+  execAndPrint(codeLines.removeRepo[opSys])
   printInfo('Removing previous frontend build')
-  execAndPrint('rm -rf ./static')
-
+  execAndPrint(codeLines.removeBuild[opSys])
   printInfo('Cloning fresh frontend repo')
-  execAndPrint(`git clone -b ${branchName} ${frontendGitUrl}`)
-
-  // printInfo('Installing frontend')
-  // execAndPrint(`cd ${dirName} && npm i && cd ..`)
-
-  // printInfo('Testing frontend')
-  // execAndPrint(`cd ${dirName} && npm test && cd ..`)
-
-  // printInfo('Building frontend')
-  // execAndPrint(`cd ${dirName} && npm run build && cd ..`)
-
+  execAndPrint(codeLines.cloneFresh)
   printInfo('Moving frontend')
-  execAndPrint(`mv ./${dirName}/build ./static`)
-
-  printInfo('Removing frontend repo')
-  execAndPrint('rm -rf ./Bears-Team-6')
+  execAndPrint(codeLines.moveRepo[opSys])
 }
 
 try {
@@ -44,4 +52,3 @@ try {
 } catch (e) {
   print(`ERROR! \n${e}`)
 }
-
